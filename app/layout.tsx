65de6b1f5e57de615f06e201a9f238bd212e6afe
Cura <adrian.cura.dev@gmail.com>
2024-08-31 00:53:57 -0300

    implement animation for the navbar with framer motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import PageTransition from "@/components/features/PageTransition";
+import StairTransition from "@/components/features/StairTransition";
 
 const JetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -23,7 +25,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={JetBrainsMono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
